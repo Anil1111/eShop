62c9586343e49780b60f89c453a0f2262c5c59d0
vishal <vjaisknit@gmail.com>
2019-05-09 20:35:47 +0530

    part-5

---
+++ eShopAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using eShopAPI.Data;
+using eShopAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+
+    // domen/api/employee
+    public class EmployeeController : ControllerBase
+    {
+        private readonly DataContext _db;
+        public EmployeeController(DataContext db)
+        {
+            _db = db;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            var EmpList=_db.Employees.ToList();
+            return Ok(EmpList);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            var employee=_db.Employees.FirstOrDefault(a=> a.id==id);
+            return Ok(employee);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
